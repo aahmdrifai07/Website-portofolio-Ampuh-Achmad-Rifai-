7e0adeb7a4ff9e62bcbb56d27fd35da772f59855
aahmdrifai07 <ahmadrifaigmg07236@gmail.com>
2026-01-05 19:44:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation (Berubah jadi X)
+        burger.classList.toggle('toggle');
+    });
+
+    // Menutup menu otomatis saat link diklik (khusus mobile)
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (nav.classList.contains('nav-active')) {
+                nav.classList.remove('nav-active');
+                burger.classList.remove('toggle');
+                navLinks.forEach(l => l.style.animation = '');
+            }
+        });
+    });
+}
+
+navSlide();
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation (Berubah jadi X)
+        burger.classList.toggle('toggle');
+    });
+
+    // Menutup menu otomatis saat link diklik (khusus mobile)
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (nav.classList.contains('nav-active')) {
+                nav.classList.remove('nav-active');
+                burger.classList.remove('toggle');
+                navLinks.forEach(l => l.style.animation = '');
+            }
+        });
+    });
+}
+
+navSlide();
